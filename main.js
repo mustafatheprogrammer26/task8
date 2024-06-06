44b1727474941972a6c8c5d6c370892b660201db
unknown <MustafaRazaShaikh>
2024-06-06 19:23:53 +0500

    task8

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = prompt("Enter your number")
+
+if (number > 0) {
+    console.log("The number is positive");
+    document.write("The number is positive")
+}
+else if (number == 0) {
+  console.log("The number is zero");
+  document.write("The number is zero")
+}
+else {
+    console.log("The number is negative");
+    document.write("The number is negative")
+
+}
